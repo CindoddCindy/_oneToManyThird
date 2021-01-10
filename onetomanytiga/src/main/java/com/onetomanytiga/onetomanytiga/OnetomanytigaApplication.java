e346b3299e3c119478b153251b7b610f7aa25f9e
CindoddCindy <cindodcindy@gmail.com>
2021-01-10 12:07:15 +0700

    tambah package

---
+++ onetomanytiga/src/main/java/com/onetomanytiga/onetomanytiga/OnetomanytigaApplication.java
@@ -0,0 +1,13 @@
+package com.onetomanytiga.onetomanytiga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnetomanytigaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnetomanytigaApplication.class, args);
+	}
+
+}
