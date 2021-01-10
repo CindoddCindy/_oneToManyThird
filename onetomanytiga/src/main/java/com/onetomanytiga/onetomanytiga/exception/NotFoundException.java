95e58de2877705b1927a261469b5d174a1292bf3
CindoddCindy <cindodcindy@gmail.com>
2021-01-10 12:38:15 +0700

    belum di run

--- onetomanytiga/src/main/java/com/onetomanytiga/onetomanytiga/exception/NotFoundException.java
+++ onetomanytiga/src/main/java/com/onetomanytiga/onetomanytiga/exception/NotFoundException.java
@@ -1,4 +1,16 @@
 package com.onetomanytiga.onetomanytiga.exception;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
-public class NotFoundException {
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "NotFoundException not found")
+public class NotFoundException  extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+
+    public NotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
--- onetomanytiga/src/main/java/com/onetomanytiga/onetomanytiga/jpa/StudentRepository.java
+++ onetomanytiga/src/main/java/com/onetomanytiga/onetomanytiga/jpa/StudentRepository.java
@@ -1,4 +1,7 @@
 package com.onetomanytiga.onetomanytiga.jpa;
 
-public interface StudentRepository {
+import com.onetomanytiga.onetomanytiga.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
 }
